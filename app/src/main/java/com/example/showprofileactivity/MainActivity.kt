8ba72dd2dde3116d344fb9fdf6ad94ca5a884fa3
kangchenghua <chenghuakang@gmail.com>
2023-03-23 12:15:12 +0100

    Add menu.

--- app/src/main/java/com/example/showprofileactivity/MainActivity.kt
+++ app/src/main/java/com/example/showprofileactivity/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.showprofileactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.mymenu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.pencil -> {
+                //
+                true
+            }
+            R.id.pencil2 -> {
+                //
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
+
