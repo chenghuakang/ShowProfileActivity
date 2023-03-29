82f3c5378fa3e5279d73ef463fb36402922be844
kangchenghua <chenghuakang@gmail.com>
2023-03-29 12:44:38 +0200

    Fix bugs.

--- app/src/main/java/com/example/showprofileactivity/MainActivity.kt
+++ app/src/main/java/com/example/showprofileactivity/MainActivity.kt
@@ -10,11 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            setContentView(R.layout.activity_main_horizontal)
-        } else {
-            setContentView(R.layout.activity_main)
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
